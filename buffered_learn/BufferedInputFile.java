08889ee567236e40c567484fdc18d237cf15b6e1
redhood <2043570808@qq.com>
2019-03-23 23:01:42 +0800

    #io BufferedReader & FileReader

---
+++ buffered_learn/BufferedInputFile.java
@@ -0,0 +1,25 @@
+package file_class.buffered_learn;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedInputFile {
+    //Throw exceptions to console
+    public static String read(String filenam) throws IOException {
+        //reading iput by line
+        BufferedReader in = new BufferedReader(new FileReader(filenam));
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while((s = in.readLine()) != null){
+            sb.append(s + "\n");
+        }
+        in.close();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(read("./src/file_class/buffered_learn/BufferedInputFile.java"));
+    }
+}
